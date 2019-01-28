aede9caa7aae4808b0a4fb412df44b05b7392297
saeedPadyab <padyabsaeed@gmail.com>
2019-01-28 20:59:13 +0300

    login first version created

---
+++ packages/login/lib/login.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Alert, StatusBar } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { languageManager } from "@app-sdk/services";
+
+import {
+  Spinner,
+  Text,
+  Button,
+  Container,
+  Image,
+  PhoneNumberInput
+} from "@app-sdk/components"; // public components
+import styles from "./style";
+import translation from "./translation";
+
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    languageManager.addToTranslation(this, translation);
+    this.state = {
+      spinner: false,
+      isValid: false,
+      phoneNumber: ""
+    };
+  }
+  onChangeText = (isValid, phoneNumber) => {
+    this.setState({ isValid: isValid, phoneNumber: phoneNumber });
+  };
+
+  signIn = async () => {};
+
+  render() {
+    return (
+      <KeyboardAwareScrollView style={styles.scrollKeyboardView}>
+        <StatusBar hidden />
+        <Container style={styles.top}>
+          <Icon name="angle-up" style={styles.topIcon} />
+          <Image source={require("./assets/m-v.png")} style={styles.topImage} />
+          <Text style={styles.topText}>
+            {languageManager.doTranslate(this, "LOGIN_TITLE")}
+          </Text>
+        </Container>
+        <Container style={styles.bottom}>
+          <PhoneNumberInput
+            onChange={(isValid, text) => this.onChangeText(isValid, text)}
+            placeholder={languageManager.doTranslate(
+              this,
+              "LOGIN_INPUT_PLACEHLODER"
+            )}
+            style={styles.bottomInput}
+          />
+          <Button style={styles.bottomBtn} onPress={this.signIn}>
+            <Spinner
+              size="small"
+              show={this.state.spinner}
+              style={styles.spinner}
+            />
+            <Text style={styles.bottomBtnText}>
+              {languageManager.doTranslate(this, "LOGIN_BTN_TEXT")}
+            </Text>
+          </Button>
+        </Container>
+      </KeyboardAwareScrollView>
+    );
+  }
+}
---
+++ packages/login/lib/translation.js
@@ -0,0 +1,20 @@
+const translation = {
+  en: {
+    LOGIN_TITLE: "We'll send you\na one time message!",
+    LOGIN_INPUT_PLACEHLODER: "Phone Number",
+    LOGIN_BTN_TEXT: "Send Code",
+    LOGIN_CONFIRMATION_CODE_TITLE:
+      "Please Enter SMS Code Sent To Your Phone Number",
+    LOGIN_CONFIRMATION_CODE__BTN_TEXT: "Send"
+  },
+  fa: {
+    LOGIN_TITLE:
+      "پس از وارد کردن شماره همراه خود \n کد اعتبارسنجی برای شما ارسال خواهد شد",
+    LOGIN_INPUT_PLACEHLODER: "شماره تلفن",
+    LOGIN_BTN_TEXT: "ارسال کد",
+    LOGIN_CONFIRMATION_CODE_TITLE: `لطفا کدی که به تلفن همراه شما ارسال شد را وارد کنید.`,
+    LOGIN_CONFIRMATION_CODE__BTN_TEXT: `ارسال`
+  }
+};
+
+export default translation;
