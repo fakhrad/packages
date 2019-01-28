f044eadfc746c9de184807eeb2b71d8440d2d7de
saeedPadyab <padyabsaeed@gmail.com>
2019-01-28 14:18:47 +0300

    tutorial confgurable done . using components version 1.0.4 and services 1.0.6

--- packages/tutorial/lib/tutorial.js
+++ packages/tutorial/lib/tutorial.js
@@ -3,23 +3,15 @@ import { StatusBar } from "react-native";
 import Swiper from "react-native-swiper";
 import { styles } from "./styles";
 import translation from "./translation";
-import { Button, Container, Image, Text } from "@app-sdk/components";
+import { Container, Image, Text, NavigateButton } from "@app-sdk/components";
 import { themeManager, languageManager } from "@app-sdk/services";
 
 export default class Tutorial extends React.Component {
   constructor(props) {
     super(props);
-    languageManager.addToTranslation(translation);
+    languageManager.addToTranslation(this, translation);
   }
-  componentDidMount() {
-    //SplashScreen.hide();
-  }
-  onLoginClick = () => {
-    this.props.onLoginRequest();
-  };
-  onSignUpClick = () => {
-    this.props.onSignupRequest();
-  };
+
   render() {
     let theme = themeManager.getAppTheme();
     const dotColor = theme["$color4"];
@@ -36,68 +28,105 @@ export default class Tutorial extends React.Component {
         >
           <Container style={styles.slide1}>
             <Image
-              source={require("./assets/graps.png")}
+              source={
+                this.props.config.image1 || require("./assets/watermlone.png")
+              }
               style={styles.image}
               resizeMode="center"
             />
             <Text style={styles.slidesTitle}>
-              {languageManager.doTranslate("TUTORIAL_FIRST_SECTION_TITLE")}
+              {languageManager.doTranslate(
+                this,
+                "TUTORIAL_FIRST_SECTION_TITLE"
+              )}
             </Text>
             <Text style={styles.slidesDescription}>
-              {languageManager.doTranslate("TUTORIAL_FIRST_SECTION_DESCRIPTION")}
+              {languageManager.doTranslate(
+                this,
+                "TUTORIAL_FIRST_SECTION_DESCRIPTION"
+              )}
             </Text>
           </Container>
           <Container style={styles.slide2}>
             <Image
-              source={require("./assets/strobires.png")}
+              source={
+                this.props.config.image2 || require("./assets/cherry.png")
+              }
               style={styles.image}
               resizeMode="center"
             />
             <Text style={styles.slidesTitle}>
-              {languageManager.doTranslate("TUTORIAL_SECOND_SECTION_TITLE")}
+              {languageManager.doTranslate(
+                this,
+                "TUTORIAL_SECOND_SECTION_TITLE"
+              )}
             </Text>
             <Text style={styles.slidesDescription}>
-              {languageManager.doTranslate("TUTORIAL_SECOND_SECTION_DESCRIPTION")}
+              {languageManager.doTranslate(
+                this,
+                "TUTORIAL_SECOND_SECTION_DESCRIPTION"
+              )}
             </Text>
           </Container>
           <Container style={styles.slide3}>
             <Image
-              source={require("./assets/cherry.png")}
+              source={
+                this.props.config.image3 || require("./assets/strobires.png")
+              }
               style={styles.image}
               resizeMode="center"
             />
             <Text style={styles.slidesTitle}>
-              {languageManager.doTranslate("TUTORIAL_THIRD_SECTION_TITLE")}
+              {languageManager.doTranslate(
+                this,
+                "TUTORIAL_THIRD_SECTION_TITLE"
+              )}
             </Text>
             <Text style={styles.slidesDescription}>
-              {languageManager.doTranslate("TUTORIAL_THIRD_SECTION_DESCRIPTION")}
+              {languageManager.doTranslate(
+                this,
+                "TUTORIAL_THIRD_SECTION_DESCRIPTION"
+              )}
             </Text>
           </Container>
           <Container style={styles.slide4}>
             <Image
-              source={require("./assets/watermlone.png")}
+              source={this.props.config.image4 || require("./assets/graps.png")}
               style={styles.image}
               resizeMode="center"
             />
             <Text style={styles.slidesTitle}>
-              {languageManager.doTranslate("TUTORIAL_FOURTH_SECTION_TITLE")}
+              {languageManager.doTranslate(
+                this,
+                "TUTORIAL_FOURTH_SECTION_TITLE"
+              )}
             </Text>
             <Text style={styles.slidesDescription}>
-              {languageManager.doTranslate("TUTORIAL_FOURTH_SECTION_DESCRIPTION")}
+              {languageManager.doTranslate(
+                this,
+                "TUTORIAL_FOURTH_SECTION_DESCRIPTION"
+              )}
             </Text>
           </Container>
         </Swiper>
         <Container style={styles.actionsContainer}>
-          <Button style={styles.actionsBtn} onPress={this.onSignUpClick}>
+          <NavigateButton
+            style={styles.actionsBtn}
+            source={this.props.config.loginPage}
+          >
             <Text style={styles.actionsBtnText}>
-              {languageManager.doTranslate("TUTORIAL_BTN_LOGIN")}
+              {languageManager.doTranslate(this, "TUTORIAL_BTN_LOGIN")}
             </Text>
-          </Button>
-          <Button style={styles.actionsBtn} onPress={this.onSignUpClick}>
+          </NavigateButton>
+
+          <NavigateButton
+            style={styles.actionsBtn}
+            source={this.props.config.signUpPage}
+          >
             <Text style={styles.actionsBtnText}>
-              {languageManager.doTranslate("TUTORIAL_BTN_SIGNUP")}
+              {languageManager.doTranslate(this, "TUTORIAL_BTN_SIGNUP")}
             </Text>
-          </Button>
+          </NavigateButton>
         </Container>
       </Container>
     );
